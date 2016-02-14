1dc40b93003a6c1d4544e9f208c23d5b4124c44e
Daniel Tan <danielkc.tan@yahoo.com>
2016-02-13 22:09:20 -0800

    wip sign up handler

--- pawalker/client/client.js
+++ pawalker/client/client.js
@@ -2,7 +2,7 @@ var trimInput = function(val) {
     return val.replace(/^\s*|\s*$/g, "");
 }
 
-Template.body.events({
+Template.Login.events({
     'click .login__submit': function (event, template) {
         event.preventDefault();
         // retrieve the input field values
@@ -15,13 +15,27 @@ Template.body.events({
         // If validation passes, supply the appropriate fields to the
         // Meteor.loginWithPassword() function.
         Meteor.loginWithPassword(email, password, function (err) {
-            if (err) {
+            if (!err) {
                 console.log("User's logged in")
             } else {
                 console.log("Login Unsuccessful")
             }
-            // The user has been logged in.
         });
-        console.log('')
+    },
+    'click .login__signup': function (event) {
     }
 });
+
+Template.SignUp.events({
+    'click .login__submit': function (event, template) {
+        event.preventDefault();
+        var email = template.find('.login__input.name').value;
+        var password = template.find('.login__input.pass').value;
+        trimInput(email);
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+    }
+});
+
